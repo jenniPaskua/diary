0fb15cf65573de8ea193066671cb250cb3c3f3c8
조지은 <jenniPaskua@gmail.com>
2022-05-18 11:30:49 +0900

    ✨ Write page

--- Home.js
+++ Home.js
@@ -5,7 +5,7 @@ import { Ionicons } from "@expo/vector-icons";
 
 const View = styled.View`
   flex: 1;
-  padding: 0px 50px;
+  padding: 0px 30px;
   padding-top: 100px;
   background-color: ${colors.bgColor};
 `;
@@ -13,6 +13,7 @@ const View = styled.View`
 const Title = styled.Text`
   color: ${colors.textColor};
   font-size: 38px;
+  font-weight: 500;
   margin-bottom: 100px;
 `;
 
@@ -27,7 +28,7 @@ const Btn = styled.TouchableOpacity`
   align-items: center;
   background-color: ${colors.btnColor};
   elevation: 5;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
 `;
 const BtnText = styled.Text``;
 
